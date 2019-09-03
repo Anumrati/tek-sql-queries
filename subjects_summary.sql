c72e719dffb3dc7c622033d1aebde18cbd1ccdab
Anumrati <48216622+Anumrati@users.noreply.github.com>
2019-09-03 17:07:23 +0200

    spróbujmy tak...

--- subjects_summary.sql
+++ subjects_summary.sql
@@ -1,23 +1,28 @@
-SELECT
-    pt.project_number || ' - ' || cr.customer_name || ' - ' || pt.project_name AS "PROJECT_NAME",    
-    ( (
-        SELECT
-            round(nvl(SUM(wt.end_time - wt.start_time) * 24, 0), 0)
-        FROM
-            work_time wt
-        WHERE
-            TO_CHAR(wt.start_time, 'yyyy/MM/dd') >= '2019/03/01'
-            AND wt.project_id = pt.project_id
-    ) + (
-        SELECT
-            round(nvl(SUM(sha.czas_opracowania), 0), 0)
-        FROM
-            sharepoint_work_time sha
-        WHERE
-            sha.id_projekty_tek = pt.project_id
-    ) ) AS "CZAS"
+with new_project as (
+	SELECT
+		pt.project_number || ' - ' || cr.customer_name || ' - ' || pt.project_name AS "PROJECT_NAME",    
+		round(nvl(SUM(wt.end_time - wt.start_time) * 24, 0), 0) AS "CZAS"
+	FROM
+		project pt
+		LEFT JOIN customer cr ON cr.customer_id = pt.customer_id
+		join work_time wt ON wt.project_id = pt.project_id
+	WHERE
+		TO_CHAR(wt.start_time, 'yyyy/MM/dd') >= '2019/03/01'
+),
+old_project as (
+	SELECT
+		pt.project_number || ' - ' || cr.customer_name || ' - ' || pt.project_name AS "PROJECT_NAME",    
+		round(nvl(SUM(sha.czas_opracowania), 0), 0) AS "CZAS"
+	FROM
+		project pt
+		LEFT JOIN customer cr ON cr.customer_id = pt.customer_id
+		join sharepoint_work_time sha ON wt.project_id = pt.project_id
+)
+SELECT 
+	np.PROJECT_NAME as PROJECT_NAME,
+	(np.CZAS + op.CZAS) as CZAS
 FROM
-    project pt
-    LEFT JOIN customer cr ON cr.customer_id = pt.customer_id
+	new_project np
+	FULL JOIN old_project ap
 ORDER BY
-    "CZAS" DESC;
+	2 DESC;
