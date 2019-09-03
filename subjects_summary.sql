f21411b1a0dca7ecd4b1abfddf731f05c6bfb6a2
Anumrati <48216622+Anumrati@users.noreply.github.com>
2019-09-03 23:13:50 +0200

    wersja finalna

--- subjects_summary.sql
+++ subjects_summary.sql
@@ -1,28 +1,35 @@
-with new_project as (
-	SELECT
-		pt.project_number || ' - ' || cr.customer_name || ' - ' || pt.project_name AS "PROJECT_NAME",    
-		round(nvl(SUM(wt.end_time - wt.start_time) * 24, 0), 0) AS "CZAS"
-	FROM
-		project pt
-		LEFT JOIN customer cr ON cr.customer_id = pt.customer_id
-		join work_time wt ON wt.project_id = pt.project_id
-	WHERE
-		TO_CHAR(wt.start_time, 'yyyy/MM/dd') >= '2019/03/01'
-),
-old_project as (
-	SELECT
-		pt.project_number || ' - ' || cr.customer_name || ' - ' || pt.project_name AS "PROJECT_NAME",    
-		round(nvl(SUM(sha.czas_opracowania), 0), 0) AS "CZAS"
-	FROM
-		project pt
-		LEFT JOIN customer cr ON cr.customer_id = pt.customer_id
-		join sharepoint_work_time sha ON wt.project_id = pt.project_id
+WITH new_project AS (
+    SELECT
+        pt.project_number,
+        round(nvl(SUM(wt.end_time - wt.start_time) * 24, 0), 0) AS "CZAS"
+    FROM
+        project     pt
+        INNER JOIN work_time   wt ON wt.project_id = pt.project_id
+    WHERE
+        to_char(wt.start_time, 'yyyy/MM/dd') >= '2019/03/01'
+    GROUP BY
+        pt.project_number
+), old_project AS (
+    SELECT
+        pt.project_number,
+        round(nvl(SUM(sha.czas_opracowania), 0), 0) AS "CZAS"
+    FROM
+        project                pt
+        INNER JOIN sharepoint_work_time   sha ON sha.id_projekty_tek = pt.project_id
+    GROUP BY
+        pt.project_number
 )
-SELECT 
-	np.PROJECT_NAME as PROJECT_NAME,
-	(np.CZAS + op.CZAS) as CZAS
+SELECT
+    pt.project_number
+    || ' - '
+    || cr.customer_name
+    || ' - '
+    || pt.project_name AS "PROJECT_NAME",
+    ( nvl(np.czas,0) + nvl(op.czas,0) ) AS "CZAS"
 FROM
-	new_project np
-	FULL JOIN old_project op ON np.PROJECT_NAME=op.PROJECT_NAME
+    project       pt
+    LEFT JOIN customer      cr ON cr.customer_id = pt.customer_id
+    LEFT JOIN new_project   np ON pt.project_number = np.project_number
+    LEFT JOIN old_project   op ON pt.project_number = op.project_number
 ORDER BY
-	2 DESC;
+    2 DESC;
